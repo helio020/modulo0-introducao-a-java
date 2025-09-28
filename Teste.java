c8691c92cbc65f8edbda202107792ee7858bf49e
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2025-09-27 21:06:55 -0300

    aula 07 - arrays de objetos, composicao e enums

--- Teste.java
+++ Teste.java
@@ -1,28 +1,29 @@
 
 void main() {
 
-	ItemCardapio item1 = new ItemCardapio(1L, "Refresco do Chaves", "Suco de limão que parece de tamarindo e tem gosto de groselha.", 2.99, 4);
+	Cardapio cardapio = new Cardapio();
 
-	var item2 = new ItemCardapio(3_000_000_000L, "Sanduíche de Presunto do Chaves", "Sanduíche de presunto simples, mas feito com muito amor.", 3.50, 2);
-	item2.definePromocao(2.99);
+	String linha = IO.readln("Digite um id de um item de cardápio: ");
+	long idSelecionado = Long.parseLong(linha);
 
-   IO.println("Nome: " + item2.nome);
-   IO.println("Em Promoção: " + item2.emPromocao);
-   IO.println("Preço com Desconto: " + item2.precoComDesconto);
+	ItemCardapio itemSelecionado = cardapio.itens[((int) idSelecionado) - 1];
 
-   if (item2.emPromocao) {
-	double porcentagemDesconto = item2.calculaPorcentagemDesconto();
+	IO.println("== Item do Cardápio ==");
+   IO.println("Id: " + itemSelecionado.id);
+   IO.println("Nome: " + itemSelecionado.nome);
+   IO.println("Descrição: " + itemSelecionado.descricao);
+   if (itemSelecionado.emPromocao) {
+	   IO.println("Item em promoção! 🤑");
+	double porcentagemDesconto = itemSelecionado.calculaPorcentagemDesconto();
+	IO.println("Preco: de " + itemSelecionado.preco + " por " + itemSelecionado.precoComDesconto);
 	IO.println("Porcentagem de desconto: " + porcentagemDesconto);
-	IO.println("Preco: de " + item2.preco + " por " + item2.precoComDesconto);
    } else {
-	IO.println("Preco: " + item2.preco);
+	IO.println("Preco: " + itemSelecionado.preco);
 	IO.println("Item não está em promoção");
    }
-
-	IO.println("Categoria: " + item2.obtemNomeCategoria());
+	IO.println("Categoria: " + itemSelecionado.obtemNomeCategoria());
 
 	IO.println("-------");
-	IO.println("Arrays");
 
 	double[] precos = new double[7];
 
@@ -36,72 +37,20 @@ void main() {
 
 	boolean[] emPromocao = { false, true, true, true, true, true, false };
 
-	IO.println("Preço do Item 3: " + precos[2]);
-	IO.println("Tamanho do array de precos: " + precos.length);
-	IO.println("Tamanho do array emPromocao: " + emPromocao.length);
+	IO.println("Soma dos preços: " + cardapio.obtemSomaDosPrecos());
+	IO.println("Total de itens em promoção: " + cardapio.obtemTotalDeItensEmPromocao());
 
-	IO.println("O segundo item tem promoção? " + emPromocao[1]);
-
-	double totalDePrecos = 0.0;
-	int i = 0;  // inicializacao de uma variavel
-	while (i < precos.length) { // comparacap
-		// IO.println("rodou " + i);
-		double preco = precos[i];
-		totalDePrecos = totalDePrecos + preco;
-		i++; // incremento
-	}
-	IO.println("Soma dos preços: " + totalDePrecos);
-
-	int totalDeItensEmPromocao = 0;
-	int j = emPromocao.length - 1;
-	do {
-		IO.println(j);
-		if (emPromocao[j]) {
-			totalDeItensEmPromocao++;
-		}
-		j--;
-	} while (j >= 0);
-	IO.println("Total de itens em promoção: " + totalDeItensEmPromocao);
-
-
-	double totalDePrecosComFor = 0.0;
-	for (int k = 0; k < precos.length; k++) {
-		double preco = precos[k];
-		totalDePrecosComFor += preco;
-	}
-	IO.println("Soma dos preços (com for): " + totalDePrecosComFor);
-
-	// IO.println(preco); // nao eh visivel
-	IO.println(i);
-	// IO.println(k); // nao eh visivel
-
-    // forEach
-	double totalDePrecosComForEach = 0.0;
-	for (double preco : precos) {
-		totalDePrecosComForEach += preco;	
-	}
-	IO.println("Soma dos preços (com for each): " + totalDePrecosComForEach);
-
-	// achar o primeiro preco que eh maior que 10.0
 	double precoLimite = 10.0;
-	double precoMaiorQueLimite = -1.0; 
-	for (double preco : precos) {
-		if (preco > precoLimite) {
-			precoMaiorQueLimite = preco;
-			break;
-		}
-	}
-	IO.println("O primeiro preço que é maior que " + precoLimite + ": " + precoMaiorQueLimite);
+	IO.println("O primeiro preço que é maior que " + precoLimite + ": " + cardapio.obtemPrimeiroPrecoMaiorQueLimite(precoLimite));
 
-IO.println("-------");
+	IO.println("-------");
 
 	// Imprimir todos os precos menores ou iguais ao limite
-	for (double preco : precos) {
-		if (preco <= precoLimite) {
-			IO.println("Preço menor que " + precoLimite + ": " + preco);
+	for (ItemCardapio item : cardapio.itens) {
+		if (item.preco <= precoLimite) {
+			IO.println("Preço menor que " + precoLimite + ": " + item.preco);
 			continue;
 		}
-		IO.println("Processamento pesado para: " + preco); // so queria processar para valores maiores que 10.0
 	}
 
 }
@@ -115,10 +64,10 @@ class ItemCardapio {
 	boolean emPromocao;
 	double preco;
 	double precoComDesconto;
-	int categoria;
+	CategoriaCardapio categoria;
 
 	//construtor
-	ItemCardapio(long id, String nome, String descricao, double preco, int categoria) {
+	ItemCardapio(long id, String nome, String descricao, double preco, CategoriaCardapio categoria) {
 		this.id = id;
 		this.nome = nome;
 		this.descricao = descricao;
@@ -131,34 +80,8 @@ class ItemCardapio {
 		return (preco - precoComDesconto) / preco * 100;
 	}
 
-	String obtemNomeCategoria() {
-		   /*
-			categorias
-
-				1 - Entradas
-				2 - Pratos Principais
-				3 - Sobremesas
-				4 - Bebidas
-			*/
-
-		String nomeCategoria;
-		switch(categoria) {
-			case 1:
-					nomeCategoria = "Entradas";
-					break;
-			case 2:
-					nomeCategoria = "Pratos Principais";
-					break;
-			case 3:
-					nomeCategoria = "Sobremesas";
-					break;
-			case 4:
-					nomeCategoria = "Bebidas";
-					break;
-			default:
-				nomeCategoria = "Não encontrada...";
-		}
-		return nomeCategoria;
+	CategoriaCardapio obtemNomeCategoria() {
+		return categoria;
 	}
 
 	void definePromocao(double precoComDesconto) {
@@ -166,4 +89,81 @@ class ItemCardapio {
 		this.precoComDesconto = precoComDesconto;
 	}
 
+}
+
+class Cardapio {
+
+	ItemCardapio[] itens;
+
+	Cardapio() {
+		var item1 = new ItemCardapio(1L, "Refresco do Chaves", 
+				"Suco de limão que parece de tamarindo e tem gosto de groselha.", 2.99, CategoriaCardapio.BEBIDAS);
+
+		var item2 = new ItemCardapio(2L, "Sanduíche de Presunto do Chaves", 
+					"Sanduíche de presunto simples, mas feito com muito amor.", 3.50, CategoriaCardapio.PRATOS_PRINCIPAIS);
+		item2.definePromocao(2.99);
+
+		var item3 = new ItemCardapio(3L, "Torta de Frango da Dona Florinda", 
+					"Torta de frango com recheio cremoso e massa crocante.", 12.99, CategoriaCardapio.PRATOS_PRINCIPAIS);
+		item3.definePromocao(10.99);
+
+		var item4 = new ItemCardapio(4L, "Pipoca do Quico", 
+					"Balde de pipoca preparado com carinho pelo Quico.", 4.99, CategoriaCardapio.PRATOS_PRINCIPAIS);
+		item4.definePromocao(3.99);
+
+		var item5 = new ItemCardapio(5L, "Água de Jamaica", 
+					"Água aromatizada com hibisco e toque de açúcar.", 2.5, CategoriaCardapio.PRATOS_PRINCIPAIS);
+		item5.definePromocao(2.0);
+
+		var item6 = new ItemCardapio(6L, "Churros do Chaves", 
+					"Churros recheados com doce de leite, clássicos e irresistíveis.", 4.99, CategoriaCardapio.SOBREMESAS);
+		item6.definePromocao(3.99);
+
+		var item7 = new ItemCardapio(7L, "Tacos de Carnitas", 
+					"Tacos recheados com carne tenra", 25.9, CategoriaCardapio.PRATOS_PRINCIPAIS);
+
+		itens = new ItemCardapio[7];
+		itens[0] = item1;
+		itens[1] = item2;
+		itens[2] = item3;
+		itens[3] = item4;
+		itens[4] = item5;
+		itens[5] = item6;
+		itens[6] = item7;
+	}
+
+	double obtemSomaDosPrecos() {
+		double totalDePrecos = 0.0;
+		for (ItemCardapio item : itens) {
+			totalDePrecos += item.preco;
+		}
+		return totalDePrecos;
+	}
+
+	int obtemTotalDeItensEmPromocao () {
+		int totalItensEmPromocao = 0;
+		for (ItemCardapio item : itens) {
+			if (item.emPromocao) {
+				totalItensEmPromocao++;
+			}
+		}
+		return totalItensEmPromocao;
+	}
+
+	double obtemPrimeiroPrecoMaiorQueLimite(double precoLimite) {
+		double precoMaiorQueLimite = -1.0; 
+		for (ItemCardapio item : itens) {
+			if (item.preco > precoLimite) {
+				precoMaiorQueLimite = item.preco;
+				break;
+			}
+		}
+		return precoMaiorQueLimite;
+	}
+
+}
+
+enum CategoriaCardapio {
+
+	ENTRADA, PRATOS_PRINCIPAIS, SOBREMESAS, BEBIDAS;
 }
