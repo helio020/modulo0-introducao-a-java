6f58ca210c12ffcf5a2a0eaff40be1ec471737e2
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2025-10-09 10:45:00 -0300

    aula 14 - praticando e arquivo csv

--- cardapio/src/mx/florinda/modelo/Cardapio.java
+++ cardapio/src/mx/florinda/modelo/Cardapio.java
@@ -2,45 +2,56 @@ package mx.florinda.modelo;
 
 import mx.florinda.modelo.isento.ItemCardapioIsento;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Cardapio {
 
     private final ItemCardapio[] itens;
 
-    public Cardapio() {
-        final ItemCardapio item1 = new ItemCardapio(1L, "Refresco do Chaves",
-                "Suco de limão que parece de tamarindo e tem gosto de groselha.", 2.99, CategoriaCardapio.BEBIDAS);
+    public Cardapio(String nomeArquivo) throws IOException {
+        Path arquivo = Path.of(nomeArquivo);
+        String conteudoArquivo = Files.readString(arquivo);
+        String[] linhasArquivo = conteudoArquivo.split("\n");
 
-        final ItemCardapio item2 = new ItemCardapio(2L, "Sanduíche de Presunto do Chaves",
-                "Sanduíche de presunto simples, mas feito com muito amor.", 3.50, CategoriaCardapio.PRATOS_PRINCIPAIS);
-        item2.setPromocao(2.99);
+        itens = new ItemCardapio[linhasArquivo.length];
 
-        final ItemCardapio item3 = new ItemCardapio(3L, "Torta de Frango da Dona Florinda",
-                "Torta de frango com recheio cremoso e massa crocante.", 12.99, CategoriaCardapio.PRATOS_PRINCIPAIS);
-        item3.setPromocao(10.99);
+        for (int i = 0; i < linhasArquivo.length; i++) {
+            String linha = linhasArquivo[i];
+            if (nomeArquivo.endsWith(".csv")) {
+                // trato o CSV
 
-        final ItemCardapio item4 = new ItemCardapioIsento(4L, "Pipoca do Quico",
-                "Balde de pipoca preparado com carinho pelo Quico.", 4.99, CategoriaCardapio.PRATOS_PRINCIPAIS);
-        item4.setPromocao(3.99);
+                String[] partes = linha.split(";");
+                long id = Long.parseLong(partes[0]);
+                String nome = partes[1];
+                String descricao = partes[2];
+                double preco = Double.parseDouble(partes[3]);
+                CategoriaCardapio categoria = CategoriaCardapio.valueOf(partes[4]);
 
-        final ItemCardapio item5 = new ItemCardapio(5L, "Água de Jamaica",
-                "Água aromatizada com hibisco e toque de açúcar.", 2.5, CategoriaCardapio.BEBIDAS);
-        item5.setPromocao(2.0);
+                ItemCardapio item;
+                boolean impostoIsento = Boolean.parseBoolean(partes[7]);
+                if (impostoIsento) {
+                    item = new ItemCardapioIsento(id, nome, descricao, preco, categoria);
+                } else {
+                    item = new ItemCardapio(id, nome, descricao, preco, categoria);
+                }
 
-        final ItemCardapio item6 = new ItemCardapioIsento(6L, "Churros do Chaves",
-                "Churros recheados com doce de leite, clássicos e irresistíveis.", 4.99, CategoriaCardapio.SOBREMESAS);
-        item6.setPromocao(3.99);
+                boolean emPromocao = Boolean.parseBoolean(partes[5]);
+                if (emPromocao) {
+                    double precoDesconto = Double.parseDouble(partes[6]);
+                    item.setPromocao(precoDesconto);
+                }
 
-        final ItemCardapio item7 = new ItemCardapioIsento(7L, "Tacos de Carnitas",
-                "Tacos recheados com carne tenra", 25.9, CategoriaCardapio.PRATOS_PRINCIPAIS);
+                itens[i] = item;
 
-        itens = new ItemCardapio[7];
-        itens[0] = item1;
-        itens[1] = item2;
-        itens[2] = item3;
-        itens[3] = item4;
-        itens[4] = item5;
-        itens[5] = item6;
-        itens[6] = item7;
+            } else if (nomeArquivo.endsWith(".json")) {
+                // trato o JSON
+            } else {
+                IO.println("Arquivo com extensão inválida: " + nomeArquivo);
+            }
+        }
+
     }
 
     public double getSomaDosPrecos() {
